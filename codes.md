71b56fb95d34aa206ac7bb6a3c1a3044f679c8be
Lewis <kokirill70@gmail.com>
2025-04-20 12:08:02 +0300

    var 2.0.1

--- codes.md
+++ codes.md
@@ -117,6 +117,22 @@ for n in range(1, 100):
             if sum(int(i) for i in s if i != ">") % n == 0:
                 print(n)
 ```
+```python
+def prime(n):
+    return all(n % d != 0 for d in range(2, int(n**0.5) + 1))
+for n in range(2, 100):
+    if prime(n):
+            s = ">" + "0" * 21 + "2" * 11 + "1" * n
+            while ">1" in s or ">2" in s or ">0" in s:
+                if ">1" in s:
+                    s = s.replace(">1", "22>", 1)
+                if ">2" in s:
+                    s = s.replace(">2", "2>", 1)
+                if ">0" in s:
+                    s = s.replace(">0", "1>", 1)
+            if sum(int(i) for i in s if i != ">") % n == 0:
+                print(n)
+```
 ## 13
 ```python
 from ipaddress import *
--- main.py
+++ main.py
@@ -1,49 +1,14 @@
-from math import dist
-from turtle import *
-from random import *
-
-
-def centroid(cluster):
-    m = []
-    for p1 in cluster:
-        sm = sum(dist(p1, p2) for p2 in cluster)
-        m.append([sm, p1])
-    return min(m)[1]
-
-
-data = []
-
-for line in open("tmp.txt"):
-    x, y = map(float, line.replace(",", ".").split())
-    data.append([x, y])
-
-cls = []
-
-while data:
-    cl = [data.pop()]
-    for p in cl:
-        sosed = [p1 for p1 in data if dist(p, p1) < 1]
-        for p1 in sosed:
-            cl.append(p1)
-            data.remove(p1)
-    cls.append(cl)
-
-cls = [cl for cl in cls if len(cl) > 20]
-print([len(cl) for cl in cls])
-
-tracer(0)
-up()
-screensize(10_000, 10_000)
-for cl in cls:
-    color = random(), random(), random()
-    for x, y in cl:
-        goto(x * 50, y * 50)
-        dot(5, color)
-update()
-exitonclick()
-
-centroids = [centroid(i) for i in cls]
-
-px = sum(x for x, y in centroids) / len(centroids)
-py = sum(y for x, y in centroids) / len(centroids)
-print(int(px * 100000), int(py * 100000))
+def prime(n):
+    return all(n % d != 0 for d in range(2, int(n**0.5) + 1))
+for n in range(2, 100):
+    if prime(n):
+            s = ">" + "0" * 21 + "2" * 11 + "1" * n
+            while ">1" in s or ">2" in s or ">0" in s:
+                if ">1" in s:
+                    s = s.replace(">1", "22>", 1)
+                if ">2" in s:
+                    s = s.replace(">2", "2>", 1)
+                if ">0" in s:
+                    s = s.replace(">0", "1>", 1)
+            if sum(int(i) for i in s if i != ">") % n == 0:
+                print(n)
